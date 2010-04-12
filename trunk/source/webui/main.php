a1ca5ded0ca397f1f6fc3166acb6e75f3975789b
mr.aburks <mr.aburks@9ef8cc84-647f-2289-51b3-36069cc5671d>
2010-04-12 14:43:56 +0000

    Started initial work on the web UI.

---
+++ trunk/source/webui/main.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!-- #BeginTemplate "template.dwt" -->
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<link rel="stylesheet" type="text/css" href="css/default.css"/>
+<!-- #BeginEditable "doctitle" -->
+<title>iMedLife Web Interface | Main</title>
+<!-- #EndEditable -->
+</head>
+
+<body>
+	<div id="banner">  </div>
+	
+	<!-- #BeginEditable "Login" -->
+	<div id="login">
+		<form action="login.php">
+			Username: <input type="text" name="username" />
+			Password: <input type="password" name="password" />
+			<input type="submit" name="login" value="Login"/>
+		</form>
+	</div>
+	<!-- #EndEditable -->
+	
+	<div id="menu">
+		<ul>
+			<li> <a href="main.php"> Main </a></li>
+			<li> <a href="#"> Patient Info </a></li>
+			<li> <a href="#"> Medical Info </a></li>
+			<li> <a href="#"> Help </a> </li>
+		</ul>
+	</div>
+	
+	<div id="content"> <!-- #BeginEditable "MainContent" -->Content <!-- #EndEditable --> </div>
+</body>
+
+<!-- #EndTemplate -->
+
+</html>
