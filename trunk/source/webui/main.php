4a6aec5f03e0c73f7fdc6cc9d67641513620c87c
mr.aburks <mr.aburks@9ef8cc84-647f-2289-51b3-36069cc5671d>
2010-04-14 14:49:33 +0000

    more work on the webui layout.

--- trunk/source/webui/main.php
+++ trunk/source/webui/main.php
@@ -12,8 +12,7 @@
 <body>
 	<div id="banner">  </div>
 	
-	<!-- #BeginEditable "Login" -->
-	<?php //if($_SESSION['loggedin'] != true) { //Show login form if the user has not logged in?>
+	<?php if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] != true) { //Show login form if the user has not logged in ?>
 	<div id="login">
 		<form action="login.php">
 			Username: <input type="text" name="username" />
@@ -23,8 +22,7 @@
 	</div>
 	<div id="create"> <a href="#"> Create Account </a></div>
 
-	<?php // } ?>
-	<!-- #EndEditable -->
+	<?php  } ?>
 	
 	<div id="menu">
 		<ul>
@@ -35,7 +33,20 @@
 		</ul>
 	</div>
 	
-	<div id="content"> <!-- #BeginEditable "MainContent" -->Content <!-- #EndEditable --> </div>
+	<div id="content"> <!-- #BeginEditable "MainContent" -->
+		<h1> Welcome</h1>
+		<p class="main"> iMedLife &trade; is an iPhone application that serves as a personal medical record, PMR, for the owner. 
+						Users can view their medical records, images, etc. directly from the iPhone. Users also have the capability of 
+						updating and uploading information to the server, all within the iPhone app! </p>
+						
+		<p class="main"> You have reached the web interface to the server. web users have all the capabilities that iPhone users have,
+						but with the additional capability of being able to access their medical records from any computer, anywhere!
+						Doctors may view and edit patient information from the web interfice as well. </p>
+						
+		<p class="notice"> Please Login or Create Account above to access your medical records.</p>
+											
+		<!-- #EndEditable --> </div>
+	<div id="footer"> Copyright &copy; 2010 | CSE 870 iMedLife Design Group - <a href="http://www.msu.edu" target="_blank">Mighigan State University</a></div>
 </body>
 
 <!-- #EndTemplate -->
