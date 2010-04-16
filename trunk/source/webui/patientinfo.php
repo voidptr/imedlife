fb01b9968d11b70422218a0232352e402aa863e2
mr.aburks <mr.aburks@9ef8cc84-647f-2289-51b3-36069cc5671d>
2010-04-16 16:44:12 +0000

    forgot a new file

---
+++ trunk/source/webui/patientinfo.php
@@ -0,0 +1,55 @@
+<?php
+	session_start();
+	include_once("../server/lib/connect.php"); //establish an initial connection to the database
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!-- #BeginTemplate "template.dwt" -->
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<link rel="stylesheet" type="text/css" href="css/default.css"/>
+<link rel="shortcut icon" href="images/favicon.ico" />
+<!-- #BeginEditable "doctitle" -->
+<title>iMedLife Web Interface | Patient Info </title>
+<!-- #EndEditable -->
+</head>
+
+<body>
+	<div id="banner">  
+		<p>
+			<a href="http://www.cse.msu.edu/~burksarm/imedlife/webui/main.php"><img id="logo" src="images/logo.png" alt="iMedLife"/></a></p>
+		<?php if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] == false) { ?>
+		<form id="login" action="../server/lib/login.php">
+			Username: <input name="username" type="text"/>
+			Password: <input name="password" type="password"/>
+					  <input name="login" type="submit" value="Login"/>
+					  
+		</form>
+		<a href="../server/lib/create.php"> Create Account </a>
+		<?php } ?>
+		
+	</div>
+	<div id="menu">
+		<ul>
+			<li> <a href="main.php"> Main </a></li>
+			<li> <a href="patientinfo.php"> Patient Info </a></li>
+			<li> <a href="#"> Medical Info </a></li>
+			<li> <a href="#"> Help </a> </li>
+		</ul>
+	</div>
+	
+	<div id="content"> <!-- #BeginEditable "MainContent" -->
+		<h1> Patient Info </h1>
+						
+		<?php if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] != true) //display message if not logged in
+		echo "<p class=\"notice\"> Please Login or Create Account above to access your medical records!";
+		echo "<img src=\"images/notice.png\" alt=\"!\"/></p>";
+		?>
+											
+		<!-- #EndEditable --> </div>
+	<div id="footer"> Copyright &copy; 2010 | CSE 870 iMedLife Design Group - <a href="http://www.msu.edu" target="_blank">Mighigan State University</a></div>
+</body>
+
+<!-- #EndTemplate -->
+
+</html>
