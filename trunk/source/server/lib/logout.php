ecf916765b115924826068c0c42bdd18829ec760
mr.aburks <mr.aburks@9ef8cc84-647f-2289-51b3-36069cc5671d>
2010-04-19 05:57:46 +0000

    A lot of work done on the server-side to accept and process requests from iPhone as well as WebUI.

--- trunk/source/server/lib/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-//logout.php - Just destroys the user's session and logs the user out.
-session_destroy();
-header("location: ../../webui/main.php");
-?>
--- trunk/source/server/lib/sync.php
+++
@@ -1,4 +0,0 @@
-<?php 
-//sync.php - Synchronizes the iPhone data with the data on the server when the app is started
-  
-
