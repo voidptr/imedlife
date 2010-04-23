13c6005aa50390e4e6f8f708413b12c5b067f171
mr.aburks <mr.aburks@9ef8cc84-647f-2289-51b3-36069cc5671d>
2010-04-23 23:32:08 +0000

    upload for webui done. FINALLY!

--- trunk/server/lib/web/upload.php
+++ trunk/server/lib/web/upload.php
@@ -5,49 +5,49 @@ include_once("../connect.php");
 
 //The Upload Function ...
 function upload(){
-	if(is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+
+	//Unset the $_SESSION['uploaded'] so we can make sure there are no errors
+	unset($SESSION['uploaded']);
 	
-	    //Make sure the filesize < maximum file size
-	    if($_FILES['userfile']['size'] < $maxsize) {
-				
-			//Get the patientID
-			$patientID = $_SESSION['sessionID'];
-			//Get the date to insert
+	if(is_uploaded_file($_FILES['userFile']['tmp_name'][0])) { 	    //make sure we actually got a file
+	    $maxSize = $_POST['MAX_FILE_SIZE'];
+	    if($_FILES['userFile']['size'][0] < $maxSize) { //Make sure the filesize < maximum file size
+			$patientID = $_SESSION['sessionID']; //Get the patientID
+			$size = getimagesize($_FILES['userFile']['tmp_name'][0]);
+			$uploadType = $size['mime'];
+			
+			$imageData =addslashes (file_get_contents($_FILES['userFile']['tmp_name'][0])); //Raw data to insert
+			$date = Date("l F d, Y");//Get the date to insert
 			
 		    //Get ready to insert
-		    $query = "INSERT INTO userUploads(patientID, noteDate, upload, uploadType) VALUES ({$_FILES['userfile']['name']}')";
+		    $query = "INSERT INTO userUploads(patientID, noteDate, upload, uploadType) VALUES ('$patientID', '$date', '$imageData', '$uploadType')";
 		
 		    // insert the image
 		    if(!mysql_query($query)) {
 		        echo "Unable to upload file";
-		    }
+		    }    
 	    }
     	else { // if the file is >= than the maximum allowed, don't proceed
-	     echo
-	      '<div>File exceeds the Maximum File limit</div>
-	      <div>Maximum File limit is '.$maxsize.'</div>
-	      <div>File '.$_FILES['userfile']['name'].' is '.$_FILES['userfile']['size'].' bytes</div>
-	      <hr />';
+	     echo "File exceeds the Maximum File limit<br/>";
+	     echo "Maximum File limit is $maxsize bytes.<br/>";
      	}
 
 	}
 }//END upload function
 
 // Make sure a file was submitted and the uploadType was set
-if(!isset($_FILES['userFile']) || !isset($_POST['uploadType']) || $_POST['uploadType'] == "Select Type") {
-	if (!isset($_FILES['userFile'])
-    	echo "<p>Please use your browser's back button and select a file</p>";
-    if (!isset($_POST['uploadType']))
-    	echo "<p>Please use your browser's back button and select an upload type</p>";
+if(!isset($_FILES['userFile'])) {
+    	echo "Please use your browser's back button and select a file";
 }
 else { //Go ahead since we got everything we needed
     try {
         upload(); //upload the file
+        $_SESSION['uploaded'] = "uploaded";		
+        header("location: ../../../webui/upload.php");
     }
     catch(Exception $e) { //Handle errors in the upload
         echo $e->getMessage();
         echo 'Sorry, could not upload file';
     }
 }
-
 ?>
