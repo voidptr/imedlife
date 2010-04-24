46c58981a2054c125f4d8fa15197c2165aed4626
mr.aburks <mr.aburks@9ef8cc84-647f-2289-51b3-36069cc5671d>
2010-04-24 15:28:14 +0000

    WebUI's Edit function modified to restrict fields from being edited. Still need to add functionality to actually edit the table.

--- server/lib/web/edit_records.php
+++ server/lib/web/edit_records.php
@@ -1,7 +1,27 @@
 <?php
+function restricted($restricted, $field) { //Determines whether or not the given field is restricted
+	$result = true;
+	if (count($restricted) === 0) return ($false); //Make sure we don't get an empty set of restricted fields.
+	//Search throught the restricted fields to see if the field exists
+	for($i=0; $i<count($restricted); $i++) {
+		if($restricted[$i] == $field) {
+			break; //When we find it, stop searching.
+		}
+		else if ($i == count($restricted)-1 && $restricted[$i] != $field)
+			$result = false; //If we've searched all the way through and haven't found the field, then it's not restricted
+	}
+	return ($result);
+}//End restricted function
+
 function editRecords($tableName) {//Displays the patient's information from the desired table in a table form.	
 	//First build up the restricted fields (i.e. fields that are not allowed to be edited.
 	$restrictedFields = array();//empty array. We will put them all in here
+	$fieldNames = ""; //Need to store the field names to pass them when editing. So we don't have to hard-code Every table.
+	$fields = array(); //We also need the fields to use for later
+	
+	//Now put all the restricted fields into the array. This can be done manually.
+	$restrictedFields[] = "patientID";
+	$restrictedFields[] = "medicalRecordID";
 									
 	$result = mysql_query("SHOW COLUMNS FROM $tableName"); //get all the fields from the chosen table
 	if($result) {
@@ -10,9 +30,14 @@ function editRecords($tableName) {//Displays the patient's information from the 
 			echo "<h3> Edit $tableName </h3>"; //Display which option has been chosen
 			echo "<form method=\"post\" action=\"../server/lib/proess.php\"><table border=1 cellspacing=0 cellpadding=0><tr>";
 	
-
+			$first = true; //just used for building the fieldNames. So we can make it tab delimited
 			while ($row = mysql_fetch_array($result)) { //read every field name from the table
 				echo "<th>$row[0]</th>"; //Display the row name as a table column header
+				$fields[] = $row[0]; //Append the fields so we can use set their names in the form
+				if ($first == true) 
+					$fieldNames .= $row[0]; //Append the field name
+				else
+					$fieldNames .= $row[0] ."\t"; //Append the field name with a tab delimiter in front of it
 			}
 			
 			//Now Display the information for the specific patient
@@ -23,14 +48,20 @@ function editRecords($tableName) {//Displays the patient's information from the 
 			if($result) { //If we actually got a record from the query, print it out to the table.
 				$record = mysql_fetch_array($result);
 				echo "<tr>"; //Create a new row in which we'll store the info.
+				
 				for($i = 0; $i < mysql_num_fields($result); $i++) { //Print each field into the table
-					echo "<td><input name=\"$row[i]\" value=\"$record[$i]\"/></td>";
+					//Only make the field editable if it is not restricted
+					if(!restricted($restrictedFields, $fields[$i]))
+						echo "<td><input name=\"$fields[$i]\" value=\"$record[$i]\"/></td>";
+					else
+						echo "<td><input disabled=\"disabled\" name=\"$fields[$i]\" value=\"$record[$i]\"/></td>"; //Don't allow user to edit if it's restricted					
 				}
 				echo "</tr>";//End the data row
 			}
 			else
 				echo "ERROR: Couldn't find result for patient using patientID=$patientID";						    	
 		        echo "</tr></table></div>"; //Close the table
+		        echo "<input type=\"hidden\" name=\"fields\" value=\"$fieldNames\"/>"; //Send the field names to be modified
 		        echo "<input type=\"hidden\" name=\"request\" value=\"edit\"/>";
 		        echo "<input name=\"ApplyChanges\" type=\"submit\" value=\"Submit Changes\" />";
                 echo "</form>";//Close the form
@@ -38,7 +69,7 @@ function editRecords($tableName) {//Displays the patient's information from the 
 		}
 	}   
     }
-//}//End viewRecords function
+//}//End editRecords function
 
 //If MedicalRecord Pressed then view the corresponding table 
 if(isset($_POST['MedicalRecord']))
