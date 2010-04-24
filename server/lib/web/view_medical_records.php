572354d8dc103b9fda6577001c54b22d8b2e3689
mr.aburks <mr.aburks@9ef8cc84-647f-2289-51b3-36069cc5671d>
2010-04-24 00:18:45 +0000

    More work done to view option in webui

--- server/lib/web/view_medical_records.php
+++ server/lib/web/view_medical_records.php
@@ -1,78 +1,37 @@
 <?php
+function viewRecords($tableName) {//Displays the patient's information from the desired table in a table form.									
+	$result = mysql_query("SHOW COLUMNS FROM $tableName"); //get all the fields from the medicalRecords table
+	if($result) {
+		if (mysql_num_rows($result) > 0) {
+			echo "<div class=\"viewtable\">";
+			echo "<h3> View $tableName </h3>"; //Display which option has been chosen
+			echo "<table border=1 cellspacing=0 cellpadding=5>";
+			
+			while ($row = mysql_fetch_array($result)) { //read every field name from the table
+				echo "<th>$row[0]</th>"; //Display the row name as a header
+			}
+			
+			//Now Display the information for the specific patient
+			$patientID = $_SESSION['patientID'];
+			$query = "SELECT * FROM $tableName WHERE patientID='$patientID'";
+			$record = mysql_fetch_array(mysql_query($query)); //Run the query and retrieve the record.
+			
+			if($record) { //If we actually got a record from the query, print it out to the table.
+				echo "<tr>"; //Create a new row in which we'll store the info.
+				for($i = 0; $i < count($record); $i++) { //Print each field into the table
+					echo "<td> $record[$i] </td>";
+				}
+				echo "</tr>";//End the row
+			}
+			else
+				echo "ERROR: Couldn't find result for patient using patientID=$patientID";
+								    	
+		    echo "</table></div>";
+		    mysql_free_result($result); //release the resource
+		}
+	}   
+}//End viewRecords function
 
-//Wah Displays the patient's Medical Record information in a table form.
-if (isset($_POST['MedicalRecord']) )										
-$result = mysql_query("SHOW COLUMNS FROM medicalRecords"); //get all the fields from the medicalRecords table
-if (mysql_num_rows($result) > 0) {
-	echo "<div class=\"viewtable\">";
-	echo "<h3> View Medical Record </h3>"; //Display which option has been chosen
-	echo "<table border=1 cellspacing=0 cellpadding=5>";
-	
-	while ($row = mysql_fetch_array($result)) { //read every field name from the table
-		echo "<th>$row[0]</th>"; //Display the row name as a header
-	}
-	
-	//TODO: Display the actual information for the patient in the table.
-						    	
-    echo "</table></div>";
-    mysql_free_result($result); //release the resource
-}
-										
-//Wah Displays the patient's Medical History information in a table form.
-if (isset($_POST['MedicalHistory']) )										
-$result = mysql_query("SHOW COLUMNS FROM medicalhistories"); //get all the fields from the medicalRecords table
-if (mysql_num_rows($result) > 0) {
-	echo "<div class=\"viewtable\">";
-	echo "<h3> View Medical Histories </h3>"; //Display which option has been chosen
-	echo "<table border=1 cellspacing=0 cellpadding=5>";
-	
-	while ($row = mysql_fetch_array($result)) { //read every field name from the table
-		echo "<th>$row[0]</th>"; //Display the row name as a header
-	}
-	
-	//TODO: Display the actual information for the patient in the table.
-						    	
-    echo "</table></div>";
-    mysql_free_result($result); //release the resource
-}
-
-
-//Wah Displays the patient's Healthcare Providers information in a table form.
-if (isset($_POST['HealthcareProviders']) )										
-$result = mysql_query("SHOW COLUMNS FROM healthcareproviders"); //get all the fields from the medicalRecords table
-if (mysql_num_rows($result) > 0) {
-	echo "<div class=\"viewtable\">";
-	echo "<h3> Healthcare Providers </h3>"; //Display which option has been chosen
-	echo "<table border=1 cellspacing=0 cellpadding=5>";
-	
-	while ($row = mysql_fetch_array($result)) { //read every field name from the table
-		echo "<th>$row[0]</th>"; //Display the row name as a header
-	}
-	
-	//TODO: Display the actual information for the patient in the table.
-						    	
-    echo "</table></div>";
-    mysql_free_result($result); //release the resource
-}
-
-
-//Wah Displays the patient's Insurance Company information in a table form.
-if (isset($_POST['InsuranceCompanyInformation']) )										
-$result = mysql_query("SHOW COLUMNS FROM insuranceinfo"); //get all the fields from the medicalRecords table
-if (mysql_num_rows($result) > 0) {
-	echo "<div class=\"viewtable\">";
-	echo "<h3> Insurance Company Information </h3>"; //Display which option has been chosen
-	echo "<table border=1 cellspacing=0 cellpadding=5>";
-	
-	while ($row = mysql_fetch_array($result)) { //read every field name from the table
-		echo "<th>$row[0]</th>"; //Display the row name as a header
-	}
-	
-	//TODO: Display the actual information for the patient in the table.
-						    	
-    echo "</table></div>";
-    mysql_free_result($result); //release the resource
-}
-
-                                        
+if(isset($_POST['MedicalRecord']))
+	viewRecords("medicalRecords");                            
 ?>
