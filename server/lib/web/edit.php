40d67f09269193e04f90ab339ae5215a8ab0861c
mr.aburks <mr.aburks@9ef8cc84-647f-2289-51b3-36069cc5671d>
2010-04-24 20:08:54 +0000

    Edit almost done. Just need to get the recordID to log to the recordChanges table. Otherwise, done!

--- server/lib/web/edit.php
+++ server/lib/web/edit.php
@@ -10,11 +10,47 @@ if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true) {//proceed if 
 	$patientID = $_SESSION['patientID']; //Get patientID into a variable for easy use
 	
 	//Get the table to modify and all the modified fields from POST
+	$tableName = $_POST['table'];
 	
-	//Modify the fields in the database
+	//Get the tableRecID to record in the recordChanges table
+	//@TODO: ACTUALLY MAKE THIS AVAILABLE FROM THE FORM!!!!!
+	$tableRecID = $_POST['tableRecID'];
+	echo "RECORD ID: : " .$tableRecID; //TESTING
 	
-	//Record the changes in the recordChanges table
-	$query = "INSERT INTO recordChanges(patientID, tableChanged, tableRecID) VALUES('$patientID', '$table', '$tableID')";
+	//Start building the query. We will finish building it in the loop
+	$query = "UPDATE $tableName SET "; //This is an update, not a regular insertion
+	
+	//Now get the list of fields from that table so we can process automatically
+	$fields = $_POST['fields'];
+	$fields = explode("\t", $fields); //Break up into the actual fields from the tab-delimited string
+			
+	//Now add the fields/values to be set in the query
+	for($i=0; $i<count($fields); $i++) {
+		$value = $_POST[$fields[$i]]; 
+		if ($value != "") {//If it is a restricted field, we better not try to update it
+			$query .= ($i == count($fields)-1) ? $fields[$i] ."='" .$value."'" : $fields[$i] ."='" .$value ."', "; //Don't put comma at end when we reach the last field
+		}
+	}
+	$query .= " WHERE patientID='$patientID'"; //Finish the query
+	
+	echo "Testing the query: <br/>" . $query; //Testing
+	
+	//Modify the record in the database
+	$success = mysql_query($query);
+	
+	if($success) {
+		//Record the changes in the recordChanges table by inserting a new transaction into it.
+		$query = "INSERT INTO recordChanges(patientID, tableChanged, tableRecID) VALUES('$patientID', '$tableName', '$tableRecID')";
+		$result = mysql_query($query);
+		
+		if($result) {
+			echo "RECORD CHANGED AND LOGGED!!";
+			//All done, redirect back to the edit page
+		}
+		else echo "DID NOT RECORD THE CHANGE!";
+	}
+	
+	
 }
 
 ?>
--- server/lib/web/edit_records.php
+++ server/lib/web/edit_records.php
@@ -1,13 +1,17 @@
 <?php
-function restricted($restricted, $field) { //Determines whether or not the given field is restricted
+$tableRecID = ""; //We're going to find the specific
+function restricted($restrictedFields, $field) { //Determines whether or not the given field is restricted
 	$result = true;
-	if (count($restricted) === 0) return ($false); //Make sure we don't get an empty set of restricted fields.
+	if (count($restrictedFields) === 0) return ($false); //Make sure we don't get an empty set of restricted fields.
 	//Search throught the restricted fields to see if the field exists
-	for($i=0; $i<count($restricted); $i++) {
-		if($restricted[$i] == $field) {
+	for($i=0; $i<count($restrictedFields); $i++) {
+		if($restrictedFields[$i] == $field) {
+			if ($field != "patientID" && strstr($field, "ID")) {//Use this to get the record ID for recording changes
+				$tableRecID = $field;
+			}
 			break; //When we find it, stop searching.
 		}
-		else if ($i == count($restricted)-1 && $restricted[$i] != $field)
+		else if ($i == count($restrictedFields)-1 && $restrictedFields[$i] != $field)
 			$result = false; //If we've searched all the way through and haven't found the field, then it's not restricted
 	}
 	return ($result);
@@ -20,24 +24,30 @@ function editRecords($tableName) {//Displays the patient's information from the 
 	$fields = array(); //We also need the fields to use for later
 	
 	//Now put all the restricted fields into the array. This can be done manually.
+	//@TODO: LIST ALL THE RESTRICTED FIELDS HERE!!!!!
 	$restrictedFields[] = "patientID";
 	$restrictedFields[] = "medicalRecordID";
+	$restrictedFields[] = "medicalHistoryID";
+	$restrictedFields[] = "insuranceInfoID";
+	$restrictedFields[] = "doctorID";
 									
 	$result = mysql_query("SHOW COLUMNS FROM $tableName"); //get all the fields from the chosen table
 	if($result) {
 		if (mysql_num_rows($result) > 0) {
 			echo "<div class=\"viewtable\">";
 			echo "<h3> Edit $tableName </h3>"; //Display which option has been chosen
-			echo "<form method=\"post\" action=\"../server/lib/proess.php\"><table border=1 cellspacing=0 cellpadding=0><tr>";
+			echo "<form method=\"post\" action=\"../server/process.php\"><table border=1 cellspacing=0 cellpadding=0><tr>";
 	
 			$first = true; //just used for building the fieldNames. So we can make it tab delimited
 			while ($row = mysql_fetch_array($result)) { //read every field name from the table
 				echo "<th>$row[0]</th>"; //Display the row name as a table column header
 				$fields[] = $row[0]; //Append the fields so we can use set their names in the form
-				if ($first == true) 
+				if ($first == true) {
+					$first = false;
 					$fieldNames .= $row[0]; //Append the field name
+				}
 				else
-					$fieldNames .= $row[0] ."\t"; //Append the field name with a tab delimiter in front of it
+					$fieldNames .= "\t" .$row[0]; //Append the field name with a tab delimiter in front of it
 			}
 			
 			//Now Display the information for the specific patient
@@ -49,23 +59,35 @@ function editRecords($tableName) {//Displays the patient's information from the 
 				$record = mysql_fetch_array($result);
 				echo "<tr>"; //Create a new row in which we'll store the info.
 				
-				for($i = 0; $i < mysql_num_fields($result); $i++) { //Print each field into the table
+				for($i = 0; $i < mysql_num_fields($result); $i++) { //Print each field into the table						
 					//Only make the field editable if it is not restricted
 					if(!restricted($restrictedFields, $fields[$i]))
 						echo "<td><input name=\"$fields[$i]\" value=\"$record[$i]\"/></td>";
-					else
-						echo "<td><input disabled=\"disabled\" name=\"$fields[$i]\" value=\"$record[$i]\"/></td>"; //Don't allow user to edit if it's restricted					
+					else {
+						echo "<td><input disabled=\"disabled\" name=\"$fields[$i]\" value=\"$record[$i]\"/></td>"; //Don't allow user to edit if it's restricted
+						
+						//Now we need to set the tableRecID. So if it's restricted we know tableRecID has been set if we're not looking at patientID
+						//Get the actualID to send now.
+						if($fields[$i] == $tableRecID){ echo "FOUND:";
+							$tableRecID = $record[$i]; //Assign the actual value instead of the field name
+						} else echo "TABLERECID: " .$tableRecID;
+					
+	
+					}				
 				}
-				echo "</tr>";//End the data row
-			}
-			else
-				echo "ERROR: Couldn't find result for patient using patientID=$patientID";						    	
+				echo "</tr>";//End the data row					    	
 		        echo "</tr></table></div>"; //Close the table
+		        echo "<input type=\"hidden\" name=\"table\" value=\"$tableName\"/>"; //Name of the table to change
+		        echo "<input type=\"hidden\" name=\"tableRecID\" value=\"$tableRecID\"/>"; //ID in the table we're modifying
 		        echo "<input type=\"hidden\" name=\"fields\" value=\"$fieldNames\"/>"; //Send the field names to be modified
 		        echo "<input type=\"hidden\" name=\"request\" value=\"edit\"/>";
-		        echo "<input name=\"ApplyChanges\" type=\"submit\" value=\"Submit Changes\" />";
-                echo "</form>";//Close the form
-                mysql_free_result($result); //release the resource
+		        echo "<input name=\"applyChanges\" type=\"submit\" value=\"Submit Changes\" />";
+	            echo "</form>";//Close the form
+	            mysql_free_result($result); //release the resource
+				
+			}
+			else
+				echo "ERROR: Couldn't find result for patient using patientID=$patientID";	
 		}
 	}   
     }
